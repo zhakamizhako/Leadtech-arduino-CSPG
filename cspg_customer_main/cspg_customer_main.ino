e2b2ebd5030dcc164128d37d18af4f6f5a211a2f
ZhakamiZhako <solon.rolandkimandre@gmail.com>
2019-06-16 01:37:52 +0800

    Added RTC Handlers to set time via serial Added RFID Reader Updated inits to accomodate necessary pinouts

--- cspg_customer_main/cspg_customer_main.ino
+++ cspg_customer_main/cspg_customer_main.ino
@@ -1,7 +1,11 @@
+#define VERSION "v0.001b" // Software Version
+#define DEV_MESSAGE "Roland Kim Andre Solon"
+#define BUILD_NUMBER "Build 0001 06/16/19"
+#define UID "DEV-0000-0001"
 //Leadtech
 //Arduino sketch for CSPG customer module
 //June 2019
-//Author: trosh
+//Author: trosh, kim
 
 //snippets used from
 //DS1302 RTC from (https://playground.arduino.cc/Main/DS1302RTC/)
@@ -11,7 +15,15 @@
 #include <Time.h>
 #include <TimeLib.h>
 #include <U8glib.h>
+#include <SPI.h>
+#include <MFRC522.h>
 
+#define TIME_HEADER  "T"   // Header tag for serial time sync message
+#define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
+
+constexpr uint8_t RST_PIN = 9;          // Configurable, see typical pin layout above
+constexpr uint8_t SS_PIN = 10;         // Configurable, see typical pin layout above
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 // Init the DS1302
 // Set pins:  CE, IO,CLK
 DS1302RTC RTC(5, 6, 7);
@@ -20,7 +32,7 @@ char strTime[9]; //Lateral time string variable to display
 char strDate[15]; //Lateral time string variable to display
 
 //u8glib
-U8GLIB_SSD1306_128X64 u8g(13, 11, 10, 9);  // SW SPI Com: SCK = 13, MOSI = 11, CS = 10, A0 = 9
+U8GLIB_SSD1306_128X64 u8g(18, 20, 26, 24);  // SW SPI Com: SCK = 13, MOSI = 11, CS = 10, A0 = 9
 
 // Init the LCD
 //   initialize the library with the numbers of the interface pins
@@ -59,9 +71,18 @@ void draw_str(unsigned int x, unsigned int y, const char *s) {
 void setup()
 {
   pinMode(8, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("[SYS][BOOT]CPSG System");
+  Serial.println(BUILD_NUMBER);
+  Serial.println(VERSION);
+  Serial.println(DEV_MESSAGE);
+  Serial.println(UID);
   //reset OLED display
   digitalWrite(8, LOW);
   digitalWrite(8, HIGH);
+  digitalWrite(2, LOW);
+//  readRAM( uint8_t *p);
+//  writeRAM(uint8_t *p);
   
   //u8glib setup
   // assign default color value
@@ -82,17 +103,17 @@ void setup()
 
   // Check clock oscillation  
   if (RTC.haltRTC())
-    draw_str("Clock stopped!");
+    draw_str("Warning! RTC has been reset!");
   else
-    draw_str("Clock working.");
+    draw_str("RTC is running.");
     
   delay(1000);
   
   // Check write-protection
   if (RTC.writeEN())
-    draw_str("Write allowed.");
+    draw_str("Write allowed");
   else
-    draw_str("Write protected.");
+    draw_str("Write protected");
 
   delay ( 1000 );
 
@@ -102,21 +123,29 @@ void setup()
   setSyncProvider(RTC.get); // the function to get the time from the RTC
   
   if(timeStatus() == timeSet)
-    draw_str(" Ok!");
+    draw_str("RTC Synced");
   else
-    draw_str(" FAIL!");
+    draw_str("RTC FAILED");
 
-  delay ( 2000 );
+  delay ( 1000 );
+
+  SPI.begin();      // Init SPI bus
+  mfrc522.PCD_Init();   // Init MFRC522
+  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
 }
 
 void loop()
 {
-
   // Display time centered on the upper line
   GetTimeInStr(strTime, hour(), minute(), second());
   GetDateInStr(strDate, weekday(), month(), day(), year());
   draw_str(strTime, strDate);
-  
+//  Serial.println(strTime);
+//  Serial.println(strDate);
+  if (Serial.available()) { // Time syncer via Serial monitor
+    Serial.println("[SYS]Serial Monitor Activity");
+    processSyncMessage();
+  }
   // Warning!
   /*if(timeStatus() != timeSet) {
     //lcd.setCursor(0, 1);
@@ -124,7 +153,21 @@ void loop()
     draw_str("RTC ERROR: SYNC!");
   }*/
 
-  delay ( 1000 ); // Wait approx 1 sec
+  //Read RFID
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  // Dump debug info about the card; PICC_HaltA() is automatically called
+  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+  draw_str("[RFID]Reading IDCARD...");
+  delay(1000);
+//  delay ( 1000 ); // Wait approx 1 sec
 }
 
 //Format time in a nice string to be displayed
@@ -217,4 +260,25 @@ void GetDateInStr(char * vString, int vWeekday, int vMonth, int vDay, int vYear)
   strcat(tStr1, tStr2);
     
   strcpy(vString, tStr1);
+}
+//Process Time via Serial Monitor function
+//Should add a verifier for this to avoid cheating.
+//Maybe the number of ticks should do?
+void processSyncMessage() {
+  unsigned long pctime;
+  const unsigned long DEFAULT_TIME = 1357041600; // Jan 1 2013
+
+  if(Serial.find(TIME_HEADER)) {
+     pctime = Serial.parseInt();
+     if( pctime >= DEFAULT_TIME) { // check the integer is a valid time (greater than Jan 1 2013)
+       setTime(pctime); // Sync Arduino clock to the time received on the serial port
+       Serial.println("[SYS][CLOCK] Time Set.");
+     }
+  }
+}
+
+time_t requestSync()
+{
+  Serial.write(TIME_REQUEST);  
+  return 0; // the time will be sent later in response to serial mesg
 }
